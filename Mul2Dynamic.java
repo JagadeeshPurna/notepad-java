717fbbf8b03024776d2f727de27e30a3f96cd7e3
JagadeeshPurna <Jagadeesh3128@gmail.com>
2024-11-08 09:41:24 +0530

    Deleted

--- Mul2Dynamic.java
+++ Mul2Dynamic.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
 Scanner scanner = new Scanner(System.in);
 float a = scanner.nextFloat();
 float b = scanner.nextFloat();
-// multiply
+
 float c = a*b;
 System.out.println("Multiplication of 2 numbers :"+ (a*b));
 }
